86a18fc394c5d1d41cb0ca2a05433fb6600b307c
Jean Tubay <jean.tubay.92@gmail.com>
2020-05-12 16:16:26 -0400

    adding bootstrap and set up files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">Col one</div>
+        <div className="col-6">Col two</div>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;

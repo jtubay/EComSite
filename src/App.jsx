bff72efeb2e91a36a02851603974e6a0f883f951
Jean Tubay <jean.tubay.92@gmail.com>
2020-05-12 17:16:48 -0400

    adding router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/NavBar'
+import ProductList from './components/ProductList'
+import Details from './components/Details'
+import Cart from './components/Cart'
+import Default from './components/Default'
 
 function App() {
   return (
     <React.Fragment>
-      <h3>Hello Gloccka</h3>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/Cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+     
     </React.Fragment>
   );
 }

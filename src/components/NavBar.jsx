bcdf63053d076ec6606dfd392eced80861a83493
Jean Tubay <jean.tubay.92@gmail.com>
2020-05-13 17:45:49 -0400

    added products component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../logo.svg'
-import styled from 'styled-components'
+import logo from '../logo.svg';
+import styled from 'styled-components';
+import { ButtonContainer } from './Button.jsx'
 
 
 export default class Navbar extends Component {
     render(){
         return(
-            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
                 <Link to='/'>
                     <img src={logo} alt="store" className="navbar-brand"/>
                 </Link>
@@ -26,27 +27,17 @@ export default class Navbar extends Component {
                         My Cart
                     </ButtonContainer>
                 </Link>
-            </nav>
+            </NavWrapper>
         )
     }
 }
 
-const ButtonContainer = styled.button`
-text-transform:capitalize;
-font-size:1.4rem;
-background: transparent;
-border:0.05rem solid var(--lightBlue);
-color:var(--lightBlue);
-border-radius:0.5rem;
-padding:0.2rem 0.5rem;
-cursor: pointer;
-margin:0.2rem 0.5rem 0.2rem 0;
-transition:all 0.5s ease-in-out;
-&:hover{
-    background:var(--lightBlue);
-    color:var(--mainBlue);
-}
-&:focus{
-    outline:none;
+const NavWrapper = styled.nav`
+background:var(--mainBlue);
+.nav-link{
+    color:var(--mainWhite)!important;
+    font-size:1.3rem;
+    text-transform:capitalize;
 }
 `
+

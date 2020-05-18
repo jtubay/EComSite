b1b15ec032d4faae6bf8db986ad3af2b355b6adf
Jean Tubay <jean.tubay.92@gmail.com>
2020-05-17 23:04:12 -0400

    added increment method

--- src/context.js
+++ src/context.js
@@ -78,13 +78,44 @@ class ProductProvider extends Component {
         })
     }
     increment = (id) => {
-        console.log('inc method')
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item=>item.id === id)
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+        product.count = product.count + 1;
+        product.total = product.count * product.price;
+
+        this.setState(()=>{
+            return {
+                cart:[...tempCart]
+            }
+        },()=>{
+            this.addTotals()
+        })
     }
     decrement = (id) => {
         console.log('de method')
     }
     removeItem = (id) => {
-        console.log('item removed')
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
+
+        tempCart = tempCart.filter(item => item.id !== id);
+        const index = tempProducts.indexOf(this.getItem(id))
+        let removedProduct = tempProducts[index];
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+
+        this.setState(() =>{
+            return {
+                cart:[...tempCart],
+                products:[...tempProducts]
+            }
+        }, () => {
+            this.addTotals();
+        })
+
     }
     clearCart = () => {
         this.setState(() => {
--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@ export const storeProducts = [
   {
     id: 1,
     title: "Google Pixel - Black",
-    img: "img/image6.jpg",
+    img: "img/product-1.png",
     price: 10,
     company: "GOOGLE",
     info:
@@ -19,7 +19,7 @@ export const storeProducts = [
   {
     id: 2,
     title: "Samsung S7",
-    img: "img/image5.jpg",
+    img: "img/product-2.png",
     price: 16,
     company: "SAMSUNG",
     info:
@@ -31,7 +31,7 @@ export const storeProducts = [
   {
     id: 3,
     title: "HTC 10 - Black",
-    img: "img/image4.jpg",
+    img: "img/product-3.png",
     price: 8,
     company: "htc",
     info:
@@ -43,7 +43,7 @@ export const storeProducts = [
   {
     id: 4,
     title: "HTC 10 - White",
-    img: "img/image3.jpg",
+    img: "img/product-4.png",
     price: 18,
     company: "htc",
     info:
@@ -55,7 +55,7 @@ export const storeProducts = [
   {
     id: 5,
     title: "HTC Desire 626s",
-    img: "img/image2.jpg",
+    img: "img/product-5.png",
     price: 24,
     company: "htc",
     info:
@@ -67,7 +67,7 @@ export const storeProducts = [
   {
     id: 6,
     title: "Vintage Iphone",
-    img: "img/image1.jpg",
+    img: "img/product-6.png",
     price: 17,
     company: "apple",
     info:
